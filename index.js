a79c0bd9d6a06c1789b4ff4afd6ecb0c1c2248eb
Jake <jakepucci26@gmail.com>
2022-08-15 16:56:50 -0600

    commit tree 1b0ed34d52bbd47cad0de92e07372f5102e30e60

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(nameInput, userInput) {
+    const formData= {
+        name: nameInput,
+        email: userInput
+    }
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+
+    })
+    .then(jsonResponse => jsonResponse.json())
+    .then(objectToDom => document.body.innerHTML = objectToDom.id)
+
+    .catch(errorMessage => document.body.innerHTML= errorMessage)
+
+ }
